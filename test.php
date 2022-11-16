4a4e1b7ac8fa61e815af8ed141b796f6d9dca1df
starscy <starscy@rambler.ru>
2022-11-16 22:54:30 +0300

    restore

---
+++ test.php
@@ -0,0 +1,23 @@
+<?php
+class Human
+{
+    public $name;
+    public $password;
+}
+
+$user1 = new Human;
+$user1->name = 'Vadim';
+$user1->password = "123";
+
+class Animal
+{
+    public $animalClass;
+    public $name;
+    public $color;
+    public $speed;
+    public $dayDie;
+}
+$kasper = new Animal();
+define('PI', 3.14);
+define('PI', 3);
+echo PI;
