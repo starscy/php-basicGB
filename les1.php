09631c0e373ba972fe74790702254265c2a6e920
starscy <starscy@rambler.ru>
2022-11-17 10:53:35 +0300

    lesson1 modernize

--- les1.php
+++ les1.php
@@ -1,9 +1,10 @@
 <?php
 $name = readline('Как вас зовут?');
-for ($i = 1; $i <= 3; $i++) {
+$taskCounts = 3;
+for ($i = 1; $i <= $taskCounts; $i++) {
     ${"task${i}"} = readline('Какая задача стоит перед вами?');
     ${"time${i}"} = readline('Сколько по времени это займет?');
+    $allTime += ${"time${i}"};
 }
 ;
-$allTime = $time1 + $time2 + $time3;
 echo "$name, сегодня у вас запланировано 3 приоритетные задачи на день:\n-$task1 ($time1" . "ч)\n-$task2 ($time2" . "ч)\n-$task3 ($time3" . "ч)\nПримерное  время выполнения плана = -$allTime" . "ч)\n";
