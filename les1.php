5d23beaf665e1855b06d066744c4a1308e58ed63
starscy <starscy@rambler.ru>
2022-11-19 13:44:55 +0300

    lesson1 add functional

--- les1.php
+++ les1.php
@@ -1,10 +1,18 @@
 <?php
-$name = readline('Как вас зовут?');
-$taskCounts = 3;
-for ($i = 1; $i <= $taskCounts; $i++) {
-    ${"task${i}"} = readline('Какая задача стоит перед вами?');
-    ${"time${i}"} = readline('Сколько по времени это займет?');
-    $allTime += ${"time${i}"};
+function get_task()
+{
+    $name = readline('Как вас зовут?');
+    $count = readline('Сколько задач у вас запланировано?');
+    $allTask = '';
+    $allTime = 0;
+    for ($i = 1; $i <= $count; $i++) {
+        ${"task${i}"} = (string) readline('Какая задача стоит перед вами?');
+        ${"time${i}"} = (int) readline('Сколько по времени это займет?');
+        $allTime += ${"time${i}"};
+        $allTask = $allTask . ${"task${i}"} . " (" . ${"time${i}"} . "ч)\n";
+    }
+    echo "$name, сегодня у вас запланировано" . " ${count} приоритетные задачи на день:
+    \n" . $allTask . "\nПримерное  время выполнения плана = $allTime" . "ч)
+    \n";
 }
-;
-echo "$name, сегодня у вас запланировано 3 приоритетные задачи на день:\n-$task1 ($time1" . "ч)\n-$task2 ($time2" . "ч)\n-$task3 ($time3" . "ч)\nПримерное  время выполнения плана = -$allTime" . "ч)\n";
+echo get_task();
